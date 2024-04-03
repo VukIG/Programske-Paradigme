de23e5ce070f5ea933303cfcd88bd68718e7120d
VukIG <vuk.ignjatovic05@gmail.com>
2024-04-03 17:34:18 +0200

    Init!

---
+++ miskotest/src/main/java/rs/co/skola/miskotest/MiskotestApplication.java
@@ -0,0 +1,13 @@
+package rs.co.skola.miskotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiskotestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiskotestApplication.class, args);
+	}
+
+}
---
+++ miskotest/src/test/java/rs/co/skola/miskotest/MiskotestApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.co.skola.miskotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiskotestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
